f2e74f8aec1ace7ba83ab892931af78c8483c2d8
cruiseqe2 <cruiseqe2@gmail.com>
2026-03-29 23:17:41 +0100

    Initial Commit

---
+++ Days/DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaysApp.swift
+//  Days
+//
+//  Created by Mark Oelbaum on 29/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
