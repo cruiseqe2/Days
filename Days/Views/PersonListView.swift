c490a84bbe172b0506cc9483b3987cac03325482
cruiseqe2 <cruiseqe2@gmail.com>
2026-04-03 17:01:15 +0100

    End of video 3.

--- Days/Views/PersonListView.swift
+++ Days/Views/PersonListView.swift
@@ -8,20 +8,106 @@
 import SQLiteData
 import SwiftUI
 
-struct PersonListView: View {
-//    @FetchAll private var people: [Person]
-    @FetchAll(Person.order(by: \.name)) private var people
-    @Dependency(\.defaultDatabase) var database
-//    @State private var newPerson = false
-    @State private var person: Person.Draft?
+@MainActor
+@Observable
+class PersonListModel {
+    enum SortField {
+        case name, birthdate
+    }
     
+    @ObservationIgnored
+    @FetchAll(Person.none) var people
+    
+    @ObservationIgnored
+    @Dependency(\.defaultDatabase) var database
+    
+    var person: Person.Draft?
+    
+    var sortAscending = true {
+        didSet {
+            Task {
+                await reloadData()
+            }
+        }
+    }
+    
+    var sortField: SortField = .name {
+        didSet {
+            Task {
+                await reloadData()
+            }
+        }
+    }
+    
+    var searchText = "" {
+        didSet {
+            Task {
+                await reloadData()
+            }
+        }
+    }
+    
+    init() {
+        Task {
+            await reloadData()
+        }
+    }
+    
+    func deleteButtonTapped(_ person: Person) {
+        withErrorReporting {
+            try database.write { db in
+                try Person
+                    .delete(person)
+                    .execute(db)
+            }
+        }
+    }
+    
+    var searchTask: Task<Void, Never>?
+    
+    func reloadData() async {
+        searchTask?.cancel()
+        searchTask = Task {
+            await withErrorReporting {
+                _ = try await $people.load(
+                    Person.all
+                        .order {
+                            switch sortField {
+                            case .name:
+                                if sortAscending {
+                                    $0.name
+                                } else {
+                                    $0.name.desc()
+                                }
+                            case .birthdate:
+                                if sortAscending {
+                                    $0.birthDate
+                                } else {
+                                    $0.birthDate.desc()
+                                }
+                            }
+                        }
+                        .where {
+                            $0.name.contains(searchText) ||
+                            $0.notes.contains(searchText)
+                        },
+                    animation: .default
+                )
+            }
+        }
+    }
+}
+
+
+struct PersonListView: View {
+    @State private var model = PersonListModel()
     var body: some View {
         NavigationStack {
             Group {
-                if people.isEmpty {
+                if model.people.isEmpty {
                     ContentUnavailableView("No People", systemImage: "person.2")
                 } else {
-                    List(people) { person in
+                    List(model.people) { person in
                         VStack(alignment: .leading) {
                             HStack {
                                 Text(person.name)
@@ -39,7 +125,7 @@ struct PersonListView: View {
                         }
                         .swipeActions(edge: .leading) {
                             Button {
-                                self.person = Person.Draft(person)
+                                model.person = Person.Draft(person)
                             } label: {
                                 Label("Edit", systemImage: "pencil")
                             }
@@ -47,34 +133,53 @@ struct PersonListView: View {
                         }
                         .swipeActions(edge: .trailing) {
                             Button(role: .destructive) {
-                                withErrorReporting {
-                                    try database.write { db in
-                                        try Person
-                                            .delete(person)
-                                            .execute(db)
-                                    }
-                                }
+                                model.deleteButtonTapped(person)
                             }
                         }
                     }
                     .listStyle(.plain)
                 }
             }
+            .searchable(text: $model.searchText, prompt: "Filter by name or note")
             .navigationTitle("People")
             .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
+                ToolbarItemGroup(placement: .topBarTrailing) {
                     Button {
-//                        newPerson = true
-                        person = Person.Draft()
+                        model.person = Person.Draft()
                     } label: {
                         Image(systemName: "plus.circle.fill")
                     }
+                    Menu {
+                        Section("Sort By") {
+                            Button {
+                                model.sortField = .name
+                            } label: {
+                                Label("Name", systemImage: model.sortField == .name ? "checkmark" : "")
+                            }
+                            Button {
+                                model.sortField = .birthdate
+                            } label: {
+                                Label("Birthdate", systemImage: model.sortField == .birthdate ? "checkmark" : "")
+                            }
+                        }
+                        Section("Order") {
+                            Button {
+                                model.sortAscending = true
+                            } label: {
+                                Label("Ascending", systemImage: model.sortAscending ? "checkmark" : "arrow.up")
+                            }
+                            Button {
+                                model.sortAscending = false
+                            } label: {
+                                Label("Descending", systemImage: !model.sortAscending ? "checkmark" : "arrow.down")
+                            }
+                        }
+                    } label: {
+                        Label("Sort", systemImage: "arrow.up.arrow.down")
+                    }
                 }
             }
-//            .sheet(isPresented: $newPerson) {
-//                PersonForm()
-//            }
-            .sheet(item: $person) { person in
+            .sheet(item: $model.person) { person in
                 PersonForm(person: person)
             }
         }
